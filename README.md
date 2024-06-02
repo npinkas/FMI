876f647fbacc806513368cb006af10543edf5481
npinkas <npinkas@frba.utn.edu.ar>
2024-06-01 23:19:58 -0300

    Subo parcial terminado

--- README.md
+++ README.md
@@ -1,7 +1,44 @@
-# ejercicio-alumno
 
-## Integrantes
+FMI (Funcional Mata Imperativo)
 
-- integrante1 (usuario github)
-- integrante2 (usuario github)
-...
+Recordatorio: Solo puede utilizar recursividad en el punto 5.b. Cualquier otra solución donde quiera aplicar recursividad no será tomada en cuenta.
+
+Enunciado general
+El fondo monetario internacional nos solicitó que modelemos su negocio, basado en realizar préstamos a países en apuros financieros. Sabemos de cada país el "ingreso per cápita" que es el promedio de lo que cada habitante necesita para subsistir, también conocemos la población activa en el sector público y la activa en el sector privado, la lista de recursos naturales (ej: "Minería", "Petróleo", "Industria pesada") y la deuda que mantiene con el FMI.
+
+El FMI es especialista en dar recetas. Cada receta combina una o más estrategias que se describen a continuación:
+prestarle n millones de dólares al país, esto provoca que el país se endeude en un 150% de lo que el FMI le presta (por los intereses)
+reducir x cantidad de puestos de trabajo del sector público, lo que provoca que se reduzca la cantidad de activos en el sector público y además que el ingreso per cápita disminuya en 20% si los puestos de trabajo son más de 100 ó 15% en caso contrario
+darle a una empresa afín al FMI la explotación de alguno de los recursos naturales, esto disminuye 2 millones de dólares la deuda que el país mantiene con el FMI pero también deja momentáneamente sin recurso natural a dicho país. No considerar qué pasa si el país no tiene dicho recurso.
+establecer un “blindaje”, lo que provoca prestarle a dicho país la mitad de su Producto Bruto Interno (que se calcula como el ingreso per cápita multiplicado por su población activa, sumando puestos públicos y privados de trabajo) y reducir 500 puestos de trabajo del sector público. Evitar la repetición de código.
+
+Se pide implementar en Haskell los siguientes requerimientos explicitando el tipo de cada función:
+
+(2 puntos)
+Representar el TAD País.
+Dar un ejemplo de cómo generar al país Namibia, cuyo ingreso per cápita es de 4140 u$s, la población activa del sector público es de 400.000, la población activa del sector privado es de 650.000, su riqueza es la minería y el ecoturismo y le debe 50 (millones de u$s) al FMI.
+
+(4 puntos) Implementar las estrategias que forman parte de las recetas del FMI. 
+
+(2 puntos)
+Modelar una receta que consista en prestar 200 millones, y darle a una empresa X la explotación de la “Minería” de un país.
+Ahora queremos aplicar la receta del punto 3.a al país Namibia (creado en el punto 1.b). Justificar cómo se logra el efecto colateral.
+
+(3 puntos) Resolver todo el punto con orden superior, composición y aplicación parcial, no puede utilizar funciones auxiliares.
+Dada una lista de países conocer cuáles son los que pueden zafar, aquellos que tienen "Petróleo" entre sus riquezas naturales.
+Dada una lista de países, saber el total de deuda que el FMI tiene a su favor.
+Indicar en dónde apareció cada uno de los conceptos (solo una vez) y justificar qué ventaja tuvo para resolver el requerimiento.
+
+(2 puntos) Debe resolver este punto con recursividad: dado un país y una lista de recetas, saber si la lista de recetas está ordenada de “peor” a “mejor”, en base al siguiente criterio: si aplicamos una a una cada receta, el PBI del país va de menor a mayor. Recordamos que el Producto Bruto Interno surge de multiplicar el ingreso per cápita por la población activa (privada y pública). 
+
+(1 punto) Si un país tiene infinitos recursos naturales, modelado con esta función
+recursosNaturalesInfinitos :: [String]
+recursosNaturalesInfinitos = "Energia" : recursosNaturalesInfinitos
+¿qué sucede evaluamos la función 4a con ese país? 
+¿y con la 4b?
+Justifique ambos puntos relacionándolos con algún concepto.
+
+
+
+
+
--- src/Library.hs
+++ src/Library.hs
@@ -1,3 +1,126 @@
 module Library where
 import PdePreludat
 
+--Parte 1
+
+type Dinero = Number
+type Poblacion = Number
+type RecursoNatural = String
+
+data Pais = UnPais {
+    ingresoPerCapita :: Dinero,
+    poblacionActivaSectorPublico :: Poblacion,
+    poblacionActivaSectorPrivado :: Poblacion,
+    recursosNaturales :: [RecursoNatural],
+    deudaMillones :: Dinero
+} deriving (Show, Eq)
+
+namibia :: Pais
+namibia = UnPais 4140 400000 650000 ["Mineria", "Ecoturismo"] 50
+
+
+--Parte 2
+type Estrategia = Pais -> Pais
+type Receta = [Estrategia]
+
+--Estrategia 1
+
+prestarNMillonesAlPais :: Number -> Estrategia 
+prestarNMillonesAlPais nMillones = modificarDeuda (aumentarPorInteres nMillones 150) 
+
+modificarDeuda :: Dinero -> Pais -> Pais
+modificarDeuda x pais = pais {deudaMillones = deudaMillones pais + x}
+
+aumentarPorInteres :: Number -> Number -> Number
+aumentarPorInteres valor porcentaje= valor * (porcentaje/100)
+
+-- Estrategia 2
+
+reducirPuestosSectorPublico :: Number -> Estrategia 
+reducirPuestosSectorPublico cantPuestos pais
+    |poblacionActivaSectorPublico pais > 100 = modificarSectorPublico 80 cantPuestos pais
+    |otherwise = modificarSectorPublico 85 cantPuestos pais
+
+modificarSectorPublico :: Number -> Number -> Estrategia 
+modificarSectorPublico ingreso cantPuestos = modificarIngresoPerCapita ingreso . modificarPuestosSectorPublico (negate cantPuestos)
+
+modificarIngresoPerCapita :: Number -> Pais -> Pais
+modificarIngresoPerCapita ingreso pais = pais {ingresoPerCapita = ingresoPerCapita pais + ingreso}
+
+modificarPuestosSectorPublico :: Number -> Pais -> Pais
+modificarPuestosSectorPublico cantPuestos pais = pais {poblacionActivaSectorPublico = poblacionActivaSectorPublico pais + cantPuestos}
+
+--Estrategia 3
+
+darExplotacionRecursoNatural ::RecursoNatural ->  Estrategia
+darExplotacionRecursoNatural recursoExplotado =  perderRecursoNatural recursoExplotado . modificarDeuda (-2)
+
+perderRecursoNatural :: RecursoNatural -> Pais -> Pais
+perderRecursoNatural recursoNatural pais = pais {recursosNaturales = encontrarRecursoNatural recursoNatural pais}
+
+encontrarRecursoNatural :: RecursoNatural -> Pais -> [RecursoNatural]
+encontrarRecursoNatural recursoExplotado pais = filter (/= recursoExplotado) (recursosNaturales pais) 
+
+--Estrategia 4
+
+blindaje :: Estrategia
+blindaje pais = (modificarPuestosSectorPublico (-500) . modificarDeuda (calcularPBI pais/2)) pais
+
+calcularPBI :: Pais -> Number
+calcularPBI pais = sumaPoblacionActiva pais * ingresoPerCapita pais
+
+sumaPoblacionActiva :: Pais -> Poblacion
+sumaPoblacionActiva pais = poblacionActivaSectorPublico pais + poblacionActivaSectorPrivado pais  
+
+--Parte 3
+
+receta1 :: Receta
+receta1 = [darExplotacionRecursoNatural "Mineria", prestarNMillonesAlPais 200]
+
+aplicarEstrategia :: Pais -> Estrategia -> Pais
+aplicarEstrategia pais estrategia = estrategia pais
+
+aplicarReceta :: Pais -> Receta -> Pais
+aplicarReceta pais receta = foldl aplicarEstrategia pais receta
+
+{-
+Es posible lograr el efecto colateral a pesar de que Haskell carezca de efecto gracias a la composicion y aplicacion parcial de las funciones auxiliares.
+
+-}
+
+--Parte 4
+
+puedenZafar :: [Pais] -> [Pais]
+puedenZafar paises = filter (elem "Petroleo" . recursosNaturales) paises
+
+
+totalDeuda :: [Pais] -> Number
+totalDeuda = sum . map deudaMillones 
+
+{-
+Funciones de Orden superior aparecen en ambos casos, por ejemplo, podemos mencionar el uso de filter en puedenZafar. La ventaja de utilziar esta funciones es que vuelven más declarativo y expresivo el codigo, ya que evitan usar bucles para recorrer listas.
+
+La composicion se puede apreciar en ambas funciones, por ejemplo en totalDeuda se compone map con sum. Esto se puede ya que la imagen de map deudaMillones es el dominio de sum. La composicion es util para usar funciones auxiliares y hacer el codigo mas compacto.
+
+La aplicacion parcial se puede ver en puedenZafar cuando se hace elem "Petroleo". Esto facilita la reutilizacion de funciones. 
+
+-}
+
+--Parte 4
+
+recetasOrdenadas :: Pais -> [Receta] ->  Bool
+recetasOrdenadas _ [] = True
+recetasOrdenadas pais (r : rs) 
+    |calcularPBI (aplicarReceta pais r) < calcularPBI(aplicarReceta pais (head rs)) = recetasOrdenadas pais rs
+    |otherwise = False
+
+--Parte 5
+
+recursosNaturalesInfinitos :: [String]
+recursosNaturalesInfinitos = "Energia" : recursosNaturalesInfinitos
+
+{-
+Si evaluamos la funcion 4.a) nunca obtendremos un resultado, ya que Haskell continuará evaluando infinitamente.
+
+En la 4.b) si devolverá un resultado ya que lo que se está evaluando en este caso es la deuda de cada país, no sus recursos naturales. Por ende, como Haskell utiliza evaluación diferida, no importa que los recursos naturales sean infinitos, ya que Haskell solo evalúa lo que necesita. 
+-}
